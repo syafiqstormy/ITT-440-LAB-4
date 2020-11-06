359e0ca097153c91917a3019b7394a12cd300828
Syafiq Shamil <syafiqstormy@gmail.com>
2020-11-06 03:00:02 +0000

    Commit for 4.2.1 - Client

--- ClientSide.c
+++ ClientSide.c
@@ -19,7 +19,7 @@ int main(int argc , char *argv[])
 		
 	server.sin_addr.s_addr = inet_addr("192.168.0.180");
 	server.sin_family = AF_INET;
-	server.sin_port = htons( 22 );
+	server.sin_port = htons( 8888 );
 
 	//Connect to remote server
 	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
